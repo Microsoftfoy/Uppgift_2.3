7ed7c59a0e7fac581603ec07328b42d6dc26982f
ercr07001 <ercr07001@E-PW05CGR0>
2024-10-10 12:38:00 +0200

    Add project files.

---
+++ Uppgift_2.3/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+namespace Uppgift_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("För hur många dagar och kilometer vill du hyra bilen?");
+            Console.WriteLine("Dagar?");
+            string Dagar = Console.ReadLine();
+            Console.WriteLine("kilometer?");
+            string Kilometer = Console.ReadLine();
+
+            Double Dagar1 = Double.Parse(Dagar);
+            Double Kilometer1 = Double.Parse(Kilometer);
+
+            Double sum = Kilometer1 * 1;
+            Double sum1 = Dagar1 * 500;
+            Double sum2 = sum + sum1 + 300;
+
+            Console.WriteLine("Total summan är:" + " " + sum2);
+
+            Console.ReadKey();
+        }
+    }
+}
